cebee321f9e0565f815d20246074f05ad8e8c2a7
aster <whirlwindaster@gmail.com>
2024-12-03 21:45:58 -0500

    use a heap for day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,58 +1,40 @@
 use std::{
+    collections::BinaryHeap,
     fs::File,
     io::{BufRead, BufReader},
-    mem::swap,
 };
 
 pub fn run() {
-    let mut left_list: Vec<u32> = vec![];
-    let mut right_list: Vec<u32> = vec![];
+    let mut left_heap: BinaryHeap<u32> = BinaryHeap::new();
+    let mut right_heap: BinaryHeap<u32> = BinaryHeap::new();
 
     read_file(
         &File::open("src/day1/input").unwrap(),
-        &mut left_list,
-        &mut right_list,
+        &mut left_heap,
+        &mut right_heap,
     );
 
-    println!("{}", calculate_difference(&left_list, &right_list));
+    println!(
+        "{}",
+        calculate_difference(left_heap.into_iter_sorted(), right_heap.into_iter_sorted())
+    );
 }
 
-fn read_file(fp: &File, l1: &mut Vec<u32>, l2: &mut Vec<u32>) {
-    let mut buf = String::with_capacity(14);
-    let mut reader = BufReader::new(fp);
-
-    loop {
-        let bytes_read = reader.read_line(&mut buf).unwrap();
-        let mut iter = buf.as_str().split_whitespace();
-
-        if bytes_read == 0 {
-            break;
-        }
-
-        insert_sorted(l1, iter.next().unwrap().parse::<u32>().unwrap());
-        insert_sorted(l2, iter.next().unwrap().parse::<u32>().unwrap());
-
-        buf.clear();
+fn read_file(fp: &File, heap1: &mut BinaryHeap<u32>, heap2: &mut BinaryHeap<u32>) {
+    for line in BufReader::new(fp).lines().map(|l| l.unwrap()) {
+        let mut vals = line.split_whitespace().map(|s| s.parse::<u32>().unwrap());
+        heap1.push(vals.next().unwrap());
+        heap2.push(vals.next().unwrap());
     }
 }
 
-fn insert_sorted<T>(vec: &mut Vec<T>, val: T)
+fn calculate_difference<U, V>(it1: U, it2: V) -> u32
 where
-    T: Ord,
+    U: IntoIterator<Item = u32>,
+    V: IntoIterator<Item = u32>,
 {
-    let mut i = vec.len();
-    vec.push(val);
-
-    while i > 0 && vec[i - 1] > vec[i] {
-        let tup = (&mut vec[i - 1..=i]).split_at_mut(1);
-        swap(&mut tup.0[0], &mut tup.1[0]);
-        i -= 1;
-    }
-}
-
-fn calculate_difference(l1: &[u32], l2: &[u32]) -> u32 {
-    l1.iter().zip(l2.iter()).fold(0, |mut acc: u32, (a, b)| {
-        acc += a.abs_diff(*b);
+    it1.into_iter().zip(it2).fold(0, |mut acc: u32, (a, b)| {
+        acc += a.abs_diff(b);
         acc
     })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-use std::env;
+#![feature(binary_heap_into_iter_sorted)]
+use clap::Parser;
 
 pub mod day1;
 pub mod day2;
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    day: u32,
+
+    #[arg(short, long, default_value_t = 1)]
+    part: u32,
+}
+
 fn main() {
-    let mut args = env::args().skip_while(|arg| arg != "-d" && arg != "--day");
-    match args
-        .nth(1)
-        .expect("expected `-d <day>` or `--day <day>` arg")
-        .as_str()
-    {
-        "1" => day1::run(),
-        "2" => day2::run(),
-        _ => panic!("expected a day number"),
-    }
+    let args = Args::parse();
+    match args.day {
+        1 => day1::run(),
+        2 => day2::run(),
+        _ => panic!("day {} not implemented", args.day),
+    };
 }
