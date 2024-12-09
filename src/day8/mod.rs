949ef5d5de3f271015e13411d5c0fc1208954550
Dominic Mayhew <dominic.mayhew@msasafety.com>
2024-12-09 09:42:20 -0500

    complete day 8

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -6,11 +6,11 @@ use std::{
 };
 
 pub fn run(part: u32) {
-    let file = File::open("src/day8/test").unwrap();
+    let file = File::open("src/day8/input").unwrap();
 
     match part {
         1 => part_one(file),
-        //2 => part_two(file),
+        2 => part_two(file),
         _ => panic!("part {part} not implemented"),
     };
 }
@@ -21,7 +21,6 @@ fn part_one(file: File) {
     let mut max: (usize, usize) = (0, 0);
 
     for (row, line) in BufReader::new(file).lines().map(|l| l.unwrap()).enumerate() {
-        max.1 = line.chars().count();
         line.chars()
             .enumerate()
             .filter(|(_, ch)| *ch != '.')
@@ -33,125 +32,108 @@ fn part_one(file: File) {
                 }
             });
         max.0 = row;
+        max.1 = line.chars().count() - 1;
     }
-
-    //println!("{:?}", buckets);
 
     for bucket in buckets.values() {
         for (i, node) in bucket.iter().enumerate() {
             for other in &bucket[(i + 1)..] {
-                //for other in bucket.iter().filter(|n| *n != node) {
                 antinodes.extend(antinode_coords(*node, *other, max));
             }
         }
     }
 
-    //println!("{:?}", antinodes);
     println!("{}", antinodes.len());
 }
 
-//fn antinode_coords(
-//    a: (usize, usize),
-//    b: (usize, usize),
-//    max: (usize, usize),
-//) -> Vec<(usize, usize)> {
-//    let mut x_vals: [Option<usize>; 2] = [None, None];
-//    let mut y_vals: [Option<usize>; 2] = [None, None];
-//
-//    if let Some(diff) = a.0.checked_sub(b.0) {
-//        x_vals[0] = b.0.checked_sub(diff);
-//        x_vals[1] = if a.0 + diff > max.0 {
-//            None
-//        } else {
-//            Some(a.0 + diff)
-//        }
-//    } else {
-//        let diff = b.0 - a.0;
-//        x_vals[1] = a.0.checked_sub(diff);
-//        x_vals[0] = if b.0 + diff > max.0 {
-//            None
-//        } else {
-//            Some(b.0 + diff)
-//        }
-//    };
-//
-//    if let Some(diff) = a.1.checked_sub(b.1) {
-//        y_vals[0] = b.1.checked_sub(diff);
-//        y_vals[1] = if a.1 + diff > max.1 {
-//            None
-//        } else {
-//            Some(a.1 + diff)
-//        }
-//    } else {
-//        let diff = b.1 - a.1;
-//        y_vals[1] = a.1.checked_sub(diff);
-//        y_vals[0] = if b.1 + diff > max.1 {
-//            None
-//        } else {
-//            Some(b.1 + diff)
-//        }
-//    };
-//
-//    //println!("{}, {}", max.0, max.1);
-//
-//    println!("{:?}, {:?}:", a, b);
-//    println!("\t{:?}", x_vals);
-//    println!("\t{:?}", y_vals);
-//
-//    x_vals
-//        .into_iter()
-//        .zip(y_vals)
-//        .filter_map(|(x, y)| {
-//            if let Some(x) = x {
-//                if let Some(y) = y {
-//                    Some((x, y))
-//                } else {
-//                    None
-//                }
-//            } else {
-//                None
-//            }
-//        })
-//        .collect::<Vec<_>>()
-//}
+fn part_two(file: File) {
+    let mut buckets: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+    let mut max: (usize, usize) = (0, 0);
+
+    for (row, line) in BufReader::new(file).lines().map(|l| l.unwrap()).enumerate() {
+        line.chars()
+            .enumerate()
+            .filter(|(_, ch)| *ch != '.')
+            .for_each(|(col, ch)| {
+                if let Some(bucket) = buckets.get_mut(&ch) {
+                    bucket.push((row, col));
+                } else {
+                    buckets.insert(ch, vec![(row, col)]);
+                }
+            });
+        max.0 = row;
+        max.1 = line.chars().count() - 1;
+    }
+
+
+    for bucket in buckets.values() {
+        for (i, node) in bucket.iter().enumerate() {
+            for other in &bucket[(i + 1)..] {
+                antinodes.extend(harmonic_antinode_coords(*node, *other, max));
+            }
+        }
+    }
+
+    println!("{}", antinodes.len());
+}
 
 fn antinode_coords(
     c1: (usize, usize),
     c2: (usize, usize),
     max: (usize, usize),
 ) -> Vec<(usize, usize)> {
-    let (x1, x2, y1, y2) = (c1.0 as f64, c2.0 as f64, c1.1 as f64, c2.1 as f64);
-    let m = (y2 - y1) / (x2 - x1);
-    let b = y1 - (m * x1);
-
-    let x_outs = [x1 + (x1 - x2), x2 + (x2 - x1)];
-    println!("{:?}, {:?}:", c1, c2);
-    println!("b: {b}");
-    println!("m: {m}");
-    println!("{:?}", x_outs);
-    let outs = x_outs
-        .into_iter()
-        .filter_map(|x| {
-            let y = m * x + b;
-            if x > (max.0 as isize) || y > (max.1 as isize) {
-                None
-            } else {
-                if let Ok(ux) = usize::try_from(x) {
-                    if let Ok(uy) = usize::try_from(y) {
-                        Some((ux, uy))
-                    } else {
-                        None
-                    }
+    let (x1, x2, y1, y2) = (c1.0 as isize, c2.0 as isize, c1.1 as isize, c2.1 as isize);
+    [(x1 + x1 - x2, y1 + y1 - y2), (x2 + x2 - x1, y2 + y2 - y1)].into_iter().filter_map(|(x, y)| {
+        if x > (max.0 as isize) || y > (max.1 as isize) {
+            None
+        } else {
+            if let Ok(ux) = usize::try_from(x) {
+                if let Ok(uy) = usize::try_from(y) {
+                    Some((ux, uy))
                 } else {
                     None
                 }
+            } else {
+                None
             }
-        })
-        .collect::<Vec<_>>();
+        }
+    }).collect::<Vec<_>>()
+}
 
-    for out in outs.iter() {
-        println!("\t{:?}", out);
+fn harmonic_antinode_coords(
+    c1: (usize, usize),
+    c2: (usize, usize),
+    max: (usize, usize),
+) -> Vec<(usize, usize)> {
+    let (x1, x2, y1, y2) = (c1.0 as isize, c2.0 as isize, c1.1 as isize, c2.1 as isize);
+    let mut out: Vec<(usize, usize)> = vec![];
+
+    let mut curr = (x2, y2);
+    while let Some(next) = {
+        let next = (curr.0 + x1 - x2, curr.1 + y1 - y2);
+        if next.0 > (max.0 as isize) || next.0 < 0 || next.1 > (max.1 as isize) || next.1 < 0 {
+            None
+        } else {
+            Some(next)
+        }
+    } {
+        out.push((next.0 as usize, next.1 as usize));
+        curr = next;
     }
 
-    outs
+    curr = (x1, y1);
+    while let Some(next) = {
+        let next = (curr.0 + x2 - x1, curr.1 + y2 - y1);
+        if next.0 > (max.0 as isize) || next.0 < 0 || next.1 > (max.1 as isize) || next.1 < 0 {
+            None
+        } else {
+            Some(next)
+        }
+    } {
+        out.push((next.0 as usize, next.1 as usize));
+        curr = next;
+    }
+
+    out
 }
